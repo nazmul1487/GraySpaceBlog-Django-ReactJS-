79e927d8abe0a79319abcca0cb1e92b2fe1860fe
nazmul1487 <nazmul15-1487@diu.edu.bd>
2020-11-14 23:44:45 +0600

    [Deploy] ready for deploy

--- grayspaceit/settings.py
+++ grayspaceit/settings.py
@@ -13,22 +13,28 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q6gt_q-os$#+1^(%kl0a!y)gg9#nbj3c56)3i8e)p=tr9=*@q)'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.str('ALLOWED_HOSTS').split(' ')
 
-
+CORS_ORIGIN_ALLOW_ALL = env.bool('CORS_ORIGIN_ALLOW_ALL', default=True)
+CORS_ORIGIN_WHITELIST = env.str('CORS_ORIGIN_WHITELIST').split(' ')
 # Application definition
 
 INSTALLED_APPS = [
@@ -45,10 +51,10 @@ INSTALLED_APPS = [
     'react_app',
     'corsheaders',
 
-
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -80,20 +86,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'grayspaceit.wsgi.application'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-
-]
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db() if env.str("DATABASE_URL", default='') else env.db('SQLITE_URL',
+                                                                           default='sqlite:///db.sqlite3')
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -124,7 +123,6 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=15),
 }
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -137,7 +135,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
@@ -155,3 +152,5 @@ LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/auth/login/'
 
 REACT_APP_DIR = os.path.join(BASE_DIR, 'react_app')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
--- react_app/src/app/api_services/authenticationsAPIService.js
+++ react_app/src/app/api_services/authenticationsAPIService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = "http://127.0.0.1:8000/api/";
+const api = "https://grayspace-blog.herokuapp.com/";
 
 const verifyToken = (token) =>{
   return axios.post(api + "token/verify/", token);
